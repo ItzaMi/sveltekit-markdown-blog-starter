5e7544790ead4dabca03878af5a7adab3f3c6308
ItzaMi <rros00@gmail.com>
2022-01-19 15:00:00 +0000

    [components] create an `seo` component and add it to two posts

---
+++ src/components/seo.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let title;
+	export let description;
+</script>
+
+<svelte:head>
+	<!-- Primary Meta Tags -->
+	<title>{title}</title>
+	<meta name="title" content={title} />
+	<meta name="description" content={description} />
+</svelte:head>
